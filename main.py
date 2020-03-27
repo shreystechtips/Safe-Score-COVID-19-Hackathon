328cb4512ee51f6d3928ca965ce4bf37d4cfd874
shrey <shreystechtips@gmail.com>
2020-03-27 10:15:23 -0700

    added main page

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ from flask_cors import CORS
 import requests
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+app = flask.Flask(__name__)
+app.Debug = os.getenv("DEBUG")
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def main():
+    message = ''
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=os.getenv("DEBUG"))
