0c12170009da641d0c42ec2e9c073503cc83083d
shrey <shreystechtips@gmail.com>
2020-03-28 13:43:28 -0700

    fixed bug with adding cities

--- scrape_inhome.py
+++ scrape_inhome.py
@@ -17,47 +17,43 @@ for x in script:
         json_object = json.loads(x.text[x.text.index(
             lookup)+len(lookup):x.text.index('NYTG.watch(')])  # this is the json object
 
-        #open('inhome.json','w').write(x.text[x.text.index(lookup)+len(lookup):x.text.index('NYTG.watch(')])
+        # open('tt.json', 'w').write(
+        #     x.text[x.text.index(lookup)+len(lookup):x.text.index('NYTG.watch(')])
 
 # clean up json_object
 for state_entry in json_object:
-    #remove unwanted top level
+    # remove unwanted top level
     for key in top_level_rm:
         state_entry.pop(key, None)
-    
-    for value in state_entry['values']:
+    temp = state_entry['values'].copy()
+    for val in temp:
+        # remove unwanted values
+        for rm in values_rm:
+            val.pop(rm, None)
 
-        #remove unwanted values    
-        for key in values_rm:
-            test = value.pop(key, None)
-            print(test)
+        # if shelter at home order is statewide, extract value data
+        if state_entry['statewide']:
+            for k, v in val.items():
+                state_entry.update({k: v})
 
-        #if shelter at home order is statewide, extract value data
-        if state_entry['statewide']:    
-            for k,v in value.items():
-                state_entry.update({k:v})
-
-        #else, sort county and city
-        if(value['geography'] == 'city'):
+        # else, sort county and city
+        if(val['geography'] == 'city'):
             state_entry['cities'] = []
             temp_dict = {}
-            for k,v in value.items():
-                temp_dict.update({k:v})
-            state_entry['values'].remove(value)
+            for k, v in val.items():
+                temp_dict[k] = v
+            # state_entry['values'].remove(val)
             state_entry['cities'].append(temp_dict)
-
-    #clean value lists (states with statewide orders)
+    # clean value lists (states with statewide orders)
     if state_entry['statewide']:
         del state_entry['values']
-    
-    #rename remaning values (states without statewide orders) to county data and remove empty lists
+
+    # rename remaning values (states without statewide orders) to county data and remove empty lists
     else:
         state_entry['county_data'] = state_entry['values']
         del state_entry['values']
         if(len(state_entry['county_data']) == 0):
             del state_entry['county_data']
-        
-#print(json_object)
 
 with open('inhome.json', 'w') as output:
-    json.dump(json_object , output)
+    json.dump(json_object, output)
